9666c97cf37362ababfca8f6bc76de45127fc7d0
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 02:31:41 +0300

    slicing

--- strings2.py
+++ strings2.py
@@ -1,21 +1,10 @@
          #01234567890123
 parrot = "Norwegian Blue"
 
-print(parrot[2].capitalize())
-print(parrot[2].replace("r", "ha!"))
-print(parrot[3])
+print(parrot[0:6]) # slicing
 
-print(parrot[-1])
-print(parrot[-0])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
-print("********")
+print()
 
-print(parrot[-11])
-print(parrot[-1])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
+print(parrot[10:])
+
+print(parrot[:])
