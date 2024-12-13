88866f88b6ead1de5993c32e2323c42bf97841b6
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 02:07:44 +0300

    negative indexing

---
+++ strings2.py
@@ -0,0 +1,21 @@
+         #01234567890123
+parrot = "Norwegian Blue"
+
+print(parrot[2].capitalize())
+print(parrot[2].replace("r", "ha!"))
+print(parrot[3])
+
+print(parrot[-1])
+print(parrot[-0])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+print("********")
+
+print(parrot[-11])
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
