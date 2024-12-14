6dce47e096fb6132fbcd57c788fa24c8c2cfdd96
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 16:59:59 +0300

    Truthy values

---
+++ truthy.py
@@ -0,0 +1,14 @@
+if "": # Evaluates to False
+    print("Not me")
+else:
+    print("I am")
+
+# 0 and any empty sequence types evaluate to False
+
+
+name = input("What is your name? ")
+
+if name:
+    print(f"Hello, {name}")
+else:
+    print("A man has no name")
