398c3a546890462fdb612db16145eceea2a1c9e5
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 04:57:39 +0300

    f-strings break on pyhton version earlier than 3.6 | also cleaner than .format

--- strings.py
+++ strings.py
@@ -6,6 +6,8 @@ print("My name is", name)
 last_name = input("Guess my last name: ")
 print(last_name, "Correct!")
 
+counter = 2
+
 age = 30
 
-print(last_name, "is", age, sep="::")
+print(last_name, f"is {age} {counter}", sep="::")
