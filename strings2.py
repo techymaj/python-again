0829943c4ec2bb4907852e0574784889e7def234
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 03:14:18 +0300

    Slicing backwards

--- strings2.py
+++ strings2.py
@@ -1,4 +1,15 @@
          #01234567890123
-parrot = "Norwegian Blue"
+parrot = "arsenal"
 
 print(parrot[::2])
+print(parrot[::-1])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+print(letters[::-1]) # Python idiom for reversing a sequence
+print(letters[::1])
+print(letters[25::-1])
+
+print()
+print(letters[-10:-13:-1])
+print(letters[-22::-1])
+print(letters[:-9:-1])
