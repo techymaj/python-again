d01b6bc3fcae85623ff44d0954680c9d7e1bd4f1
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 03:28:03 +0300

    Slicing Idioms & IndexOutOfRange errors

--- strings2.py
+++ strings2.py
@@ -6,10 +6,7 @@ print(parrot[::-1])
 
 letters = "abcdefghijklmnopqrstuvwxyz"
 print(letters[::-1]) # Python idiom for reversing a sequence
-print(letters[::1])
-print(letters[25::-1])
-
-print()
-print(letters[-10:-13:-1])
-print(letters[-22::-1])
-print(letters[:-9:-1])
+print(letters[-4:]) # Last 4 items
+print(letters[-1:]) # Last item
+print(letters[:1]) # First item. No error if sequence is empty
+print(letters[:4]) # First 4 items
