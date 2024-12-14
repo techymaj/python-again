07e00b6c4e565142f97c35517b9692ca49e49861
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 20:20:28 +0300

    If challenge solution

---
+++ if_challenge.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+if 18 <= age < 31:
+    print("Welcome to the Bahamas!")
+else:
+    print("Sorry, but you do not qualify for our holiday program")
