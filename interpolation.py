115e68f52329a4662cae90bc0d639f1c6b611596
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-14 13:36:13 +0300

    Control Flow Intro

--- interpolation.py
+++ interpolation.py
@@ -5,3 +5,11 @@ years = "years"
 months = "months"
 
 print("My age is %d %s, %d %s" %(age, years, 6, months))
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        pass
