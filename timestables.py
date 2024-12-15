254a4d322d9aabd9c6cf8ce0b11482ce0b27ce5c
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-15 23:12:37 +0300

    timestable challenge

--- timestables.py
+++ timestables.py
@@ -2,3 +2,9 @@ for x in range(1, 13):
     for y in range(1, 13):
         print(f"{x} * {y} = {x * y}")
     print("-" * 12)
+
+
+for char in "Majaliwa M. Wilfried":
+    if char == "a":
+        continue
+    print(char)
