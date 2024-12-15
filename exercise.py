c0f603ccaa6f488843a2ae95067aab2956637007
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-15 18:25:09 +0300

    timestables, ranges | more for loops

---
+++ exercise.py
@@ -0,0 +1,8 @@
+string = """Alright, but apart from the Sanitation, 
+the Medicine, Education, Wine, Public Order, Irrigation, 
+Roads, the Fresh-Water System, and Public Health, 
+what have the Romans ever done for us?"""
+
+for character in string:
+    if character.isupper():
+        print(character)
---
+++ timestables.py
@@ -0,0 +1,4 @@
+for x in range(1, 13):
+    for y in range(1, 13):
+        print(f"{x} * {y} = {x * y}")
+    print("-" * 12)
