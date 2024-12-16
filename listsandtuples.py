12d71fc7babaf62c0b42a15ad02a7f64203fbfc8
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-16 13:13:19 +0300

    Lists & Tuples Intro

---
+++ listsandtuples.py
@@ -0,0 +1,11 @@
+computer_parts = ["Monitor", "Keyboard", "Mouse"] # List, mutable
+
+sub_list = computer_parts[:2]
+print(sub_list)
+
+body_parts = "Head", "Shoulder", "Knees", "Toes" # Tuple, Immutable
+sub_tuple = body_parts[:2]
+print(sub_tuple)
+
+print(type(computer_parts))
+print(type(body_parts))
