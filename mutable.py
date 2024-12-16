f7a5ef5ee13140ee6ca295ca7828eb72b1b8d2c9
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-16 14:48:38 +0300

    More sequence operations

--- mutable.py
+++ mutable.py
@@ -14,3 +14,14 @@ print(wider_list)
 
 print(f"Wider list ID: {id(wider_list)}")
 print(f"shopping list ID: {id(shopping_list)}")
+
+del wider_list[3::]
+print(wider_list)
+
+extended_list = [1, 2, 3, 4, 5]
+extended_list.extend(wider_list)
+print(extended_list)
+
+print(extended_list is wider_list)
+
+print("Majaliwa".count("a"))
