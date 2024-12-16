3613f79e9d9fc75f0113e0f2a25de453b82e98d1
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-17 01:11:55 +0300

    A good lesson on list comprehension

--- slice_replace.py
+++ slice_replace.py
@@ -18,3 +18,32 @@ numbers.pop(5)
 print(numbers)
 numbers.pop(5)
 print(numbers)
+
+
+DATA = [2, 4, 101, 100, 123, 133, 144, 112, 133, 200, 201, 404, 500]
+
+for data in DATA[:]: # enumerate over a copy
+    if data not in range(140, 201):
+        print(f"Removing {data}")
+        DATA.remove(data)
+else:
+    print(DATA)
+
+# using a list comprehension
+# syntax: [expression for item in iterable if condition]
+# apply this expression for each item in the iterable that matches the condition
+DATA = [2, 4, 101, 100, 123, 133, 144, 112, 133, 200, 201, 404, 500]
+DATA = [data + 1 for data in DATA if data in range(140, 201)]
+print("Final DATA:", DATA)
+
+my_awesome_list = [x for x in range(11) if x % 2 == 0 and x != 0]
+print(my_awesome_list)
+
+first_name = [character.upper() for character in "Wilfried"]
+print(first_name)
+
+lucky_numbers = ["even" if x % 2 == 0 else "odd" for x in range(11)]
+print(lucky_numbers)
+
+name = "wilfried".upper()
+print(name)
