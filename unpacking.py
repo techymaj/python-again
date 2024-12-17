167f89a73fdd06b9e7506f461fbc1078df34238b
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-18 01:47:31 +0300

    Priactical uses for unpacking tuples

--- unpacking.py
+++ unpacking.py
@@ -8,7 +8,19 @@ names = ["maria", "jax", "brian"]
 m, j, br = names
 print(m, j, br, sep=", ")
 
-names.append("king")
-print(names)
-m, j, br = names # error: too many values to unpack (expected 3)
-print(m, j, br, sep=", ")
+# names.append("king")
+# print(names)
+# m, j, br = names # error: too many values to unpack (expected 3)
+# print(m, j, br, sep=", ")
+
+# each time around the loop, i and character are unpacked from the tuple
+# that enumerate creates
+for i, character in enumerate("abcdefg"):
+    print(i, character)
+print("-" * 80)
+for t in enumerate("abcdefg"):
+    i, character = t # same as above, just longer
+    print(i, character)
+
+for t in enumerate("abcdefg"):
+    print(t)
