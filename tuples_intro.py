b761f1b1a8191d0ddd4ad48f541baa5527a246ae
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-18 01:11:53 +0300

    More on tuples and the list() constructor

--- tuples_intro.py
+++ tuples_intro.py
@@ -1,2 +1,17 @@
 t = 1, 2, 3, 4, 5
 print(t)
+
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[-1])
+
+# metallica[-2] = "Not Metallica"
+metallica2 = list(metallica)
+metallica2[-2] = "Not Metallica"
+print(metallica2)
