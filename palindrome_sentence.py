c8cf60447a76d518d0bd9c7265b0da5f89c06e2f
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-18 16:57:44 +0300

    sentence_palindrome challenge tweaks

--- palindrome_sentence.py
+++ palindrome_sentence.py
@@ -13,7 +13,7 @@ def palindrome_sentence(sentence):
             c for c in new_sentence if c.isalnum() is True
         ))
         print("Alphanumeric characters detected:")
-        print(f"Removing alphanumeric character: {has_non_alnum_chars}")
+        print(f"After removing alphanumeric characters: {has_non_alnum_chars}")
         if has_non_alnum_chars == has_non_alnum_chars[::-1]:
             return f"{sentence} is a palindrome"
         else:
