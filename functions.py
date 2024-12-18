1fcee34ccc2d702020e53312be192fb314d5a2fe
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-19 01:19:48 +0300

    Docstrings challenge

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 def is_palindrome(word):
+    """
+    Checks whether the word is a palindrome.
+    :param word: The word to check.
+    :return: A friendly message: `str`, that confirms whether word is a palindrome or not.
+    """
     word = word.casefold()
     return f"{word} is a palindrome" if word == word[::-1] else f"{word} is not a palindrome"
 
