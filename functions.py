85cd629224512f2cdeeba35a898e821e9b509d3a
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-18 15:43:28 +0300

    Fun with palindromes

--- functions.py
+++ functions.py
@@ -1,17 +1,11 @@
 def is_palindrome(word):
     word = word.casefold()
-    print(word, end=": ")
-    return True if word[:] == word[::-1] else False
+    return f"{word} is a palindrome" if word == word[::-1] else f"{word} is not a palindrome"
 
 
-arsenal = is_palindrome("Arsenal")
-print(arsenal)
-
-anna = is_palindrome("Anna")
-print(anna)
-
-bob = is_palindrome("B.O.B")
-print(bob)
-
-majax = is_palindrome("Majax")
-print(majax)
+while True:
+    word_to_check = input("Enter a word to check: (Qq to quit) ")
+    if word_to_check == "Q".casefold():
+        break
+    check_palindromeness = is_palindrome(word_to_check)
+    print(check_palindromeness)
