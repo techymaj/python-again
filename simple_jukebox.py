92fa43c81d20ba30b6acd11731be24e2121b7418
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-18 07:57:13 +0300

    Crashing the simple jukebox challenge

--- simple_jukebox.py
+++ simple_jukebox.py
@@ -1,22 +1,35 @@
 from nested_indexing import albums
 
-while True:
+SONGS_LIST_INDEX = 3
+CHOSEN_TRACK_INDEX = 1
+
+def album_tracks(tracks):
+    for track, (track_number, song_title) in enumerate(tracks):
+        print(track_number, song_title, sep=" - ")
+        if track == len(tracks) - 1:
+            play = int(input("Choose a song to play: ")) - 1
+            if play not in range(len(tracks)):
+                print("Invalid choice - taking you back to album list")
+                break
+            else:
+                playing = tracks[play][CHOSEN_TRACK_INDEX]
+                print(f"\nNow playing: {playing}")
+
+
+def now_playing():
     print("***JUKEBOX***")
-    for index, album in enumerate(albums):
-        title, artist, year, songs = album
+    for index, (title, artist, year, songs) in enumerate(albums):
         print(f"{index + 1} - {title}")
         if index == len(albums) - 1:
-            option = int(input("Choose an album: ")) - 1
-            tracks = albums[option][3]
+            chosen_album = int(input("Choose an album: ")) - 1
+            tracks = albums[chosen_album][SONGS_LIST_INDEX]
             print(f"\nChosen album: {title}")
-            for track, record in enumerate(tracks):
-                track_number, song_title = record
-                print(track_number, song_title, sep=" - ")
-                if track == len(tracks) - 1:
-                    play = int(input("Choose a song to play: ")) - 1
-                    playing = tracks[play][1]
-                    print(f"\nPlaying: {playing}")
+            album_tracks(tracks)
     print("-" * 50)
+
+
+while True:
+    now_playing()
     another_song = input("Play another song? Y/n ")
     if another_song == "Y".casefold():
         continue
