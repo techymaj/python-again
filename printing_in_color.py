d57a41f93e1ae457a6fc4b44b5b592138f6750ae
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-19 15:24:06 +0300

    Printing in color

--- printing_in_color.py
+++ printing_in_color.py
@@ -1,16 +1,18 @@
 from color_codes import *
 
-def print_in_color(effect: str, text: str, reset: str = RESET) -> None:
+def print_in_color(*effects: "A list of styles", text: str, reset: str = RESET) -> None:
     """
-    Prints a given text in the color specified by `effect`
+    Prints a given text in the color specified by `effect`.
 
-    :param effect: The color to print in
-    :param text: The text to apply the color to
-    :param reset: Resets the effect to the terminal default
+    :param effects: A list of the various styles to apply. e.g
+        `BLUE, BOLD, UNDERLINE`
+    :param text: The text to apply the color to.
+    :param reset: Resets the effect to the terminal default.
     """
-    print(f"{effect}", f"{text}", f"{reset}")
+    combined_effects = ''.join(effects)
+    print(f"{combined_effects}", f"{text}", f"{reset}")
 
-print_in_color(BLUE, "Blue is the warmest color")
+print_in_color(BLUE, text="Blue is the warmest color")
 print("What about ism....")
-print_in_color(RED, "RED DAWN!!!")
-print_in_color(CYAN, "Kiwaani")
+print_in_color(RED, UNDERLINE, BOLD, text="RED DAWN!!!")
+print_in_color(CYAN, text="Kiwaani")
