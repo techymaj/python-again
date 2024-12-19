6ea7b14363b94f17f21a5230752484019460c4bd
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-19 12:33:17 +0300

    Type Hints are a specific use case of Function Annotations

--- generics_maybe.py
+++ generics_maybe.py
@@ -1,3 +1,6 @@
+from typing import Optional, TypeVar, List
+
+
 def sum_of_floats(it: list[str]) -> float:
     """
     Sums up a list of strings that are numbers
@@ -10,7 +13,28 @@ def sum_of_floats(it: list[str]) -> float:
         appended.append(int(i))
     return sum(appended)
 
+
 # The IDE will complain it's not ["5", "4", "3", "2", "1"]
 # but still be able tu run the code. so not strict generics
 sums = sum_of_floats([5, 4, 3, 2, 1])
 print(sums)
+
+
+def greet(name: Optional[str] = None) -> str:
+    if name:
+        return f"Hello, {name}!"
+    return "Hello, stranger!"
+
+
+print(greet("Wil"))
+print(greet())
+
+T = TypeVar('T')  # A generic type variable
+
+
+def reverse_list(items: List[T]) -> List[T]:
+    return items[::-1]
+
+
+print(reverse_list([1, 2, 3]))  # [3, 2, 1]
+print(reverse_list(["a", "b"]))  # ["b", "a"]
