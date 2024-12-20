aa274846b1cc9b799646b9484fd5ae440bbded27
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-21 01:28:30 +0300

    Character Counter Challenge

---
+++ character_counter.py
@@ -0,0 +1,27 @@
+# We need an empty dictionary, to store and display the letter frequencies.
+word_count = {}
+
+# Text string
+text = "Later in the course, you'll see how to use the collections Counter class."
+
+# Your code goes here ...
+split_text = text.split()
+join_text = "".join(split_text)
+lower_case_text = join_text.casefold()
+remove_alnum = "".join(
+    (c for c in lower_case_text if c.isalnum() is True)
+)
+
+for character in remove_alnum:
+    if character not in word_count:
+        # key doesn't exist, create key & set default count to 1
+        word_count.setdefault(character, 1)
+    else:
+        # key exists, update count
+        update = word_count.get(character) + 1
+        word_count.update({character: update})
+
+
+# Printing the dictionary
+for letter, count in sorted(word_count.items()):
+    print(letter, count)
