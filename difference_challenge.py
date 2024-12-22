62ec7ae7e26e3c22f016d348edd28c69bbfb195b
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 11:56:47 +0300

    Difference coding challenge

---
+++ difference_challenge.py
@@ -0,0 +1,21 @@
+favourites = {'door screen',
+              'frying pan',
+              'roller blind',
+              'football',
+              'coffee grinder',
+              'bush hat',
+              'stirling engine',
+              'cachemira cd',
+              'shirt',
+              }
+
+basket = {'garlic crusher',
+          'stirling engine',
+          'frying pan',
+          'shirt',
+          'bush hat',
+          }
+
+# Add your code here.
+favourites.difference_update(basket)
+suggestions:set = favourites
