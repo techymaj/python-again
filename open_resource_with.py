86e6d77196221b52b0662ca9baf79bbb7eaa5453
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-22 16:21:43 +0300

    other ways to read data and the seek() method

--- open_resource_with.py
+++ open_resource_with.py
@@ -1,3 +1,15 @@
 with open("/Users/mbuto/Downloads/Jabberwocky.txt", "r") as jabber:
-    for line in jabber:
-        print(line, end="")
+    data_readlines = jabber.readlines() # reads all lines including new-lines
+
+    jabber.seek(0) # reset pointer
+    data_readline = jabber.readline() # reads the first line including new-lines
+
+    jabber.seek(0) # reset pointer
+    data_read = jabber.read() # reads the entire file
+
+    print(data_readlines)
+    print("*" * 120)
+    print(data_readline)
+    print("*" * 120)
+    print(data_read)
+    print("*" * 120)
