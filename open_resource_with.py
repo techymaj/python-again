3f4f25c5eca218b8b81e924369c67af28eab2a23
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-23 11:38:40 +0300

    using strip() in text

--- open_resource_with.py
+++ open_resource_with.py
@@ -1,15 +1,9 @@
 with open("/Users/mbuto/Downloads/Jabberwocky.txt", "r") as jabber:
-    data_readlines = jabber.readlines() # reads all lines including new-lines
+    data_readline = jabber.readline().rstrip() # reads the first line
+print(data_readline)
 
-    jabber.seek(0) # reset pointer
-    data_readline = jabber.readline() # reads the first line including new-lines
 
-    jabber.seek(0) # reset pointer
-    data_read = jabber.read() # reads the entire file
+chars = "'Twas ev"
+no_chars = data_readline.strip(chars) # strip any leading or trailing characters that are in chars
 
-    print(data_readlines)
-    print("*" * 120)
-    print(data_readline)
-    print("*" * 120)
-    print(data_read)
-    print("*" * 120)
+print(no_chars)
