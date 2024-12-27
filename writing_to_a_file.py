07103f67120d226b34105bdd6d3bf832b64863e7
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-27 18:09:53 +0300

    Writing to files

--- writing_to_a_file.py
+++ writing_to_a_file.py
@@ -2,5 +2,13 @@ with open("write_to_me.txt", "w") as lets:
     lets.write("To God be the glory")
 
 
+with open("write_to_me.txt", "a") as text:
+    two = 2
+    text.write(f"{two}")
+
+with open("write_to_me.txt", "a") as text:
+    print(f"\n{12}", file=text)
+
+
 with open("write_to_me.txt") as text:
-    print(text.read(6))
+    print(text.read())
