30055204e34171ef65d58dcfb9b9858d6d3f8dfd
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-30 22:17:59 +0300

    Bad Code challenge

--- bad_code.py
+++ bad_code.py
@@ -1,8 +1,7 @@
 area = 0
 
 def area_of_square(length: float):
-    global area
-    area = length * length
+    return length * length
 
-area_of_square(30)
-print(f"The area is: {area}")
+sq_area = area_of_square(30)
+print(f"The area is: {sq_area}")
