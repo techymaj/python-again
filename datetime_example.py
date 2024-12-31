731806edf98a8081c59829ed9e76fda9733114e2
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-31 12:26:00 +0300

    prefer now to today

--- datetime_example.py
+++ datetime_example.py
@@ -1,9 +1,7 @@
 import datetime as date_objects
 from datetime import datetime
 
-today = datetime.today()
-kati = datetime.now()
+kati = datetime.now()  # preferred way
 utc_now = datetime.utcnow()
-_time = date_objects.time.fromisoformat("11:30")
 
-print(today, _time, kati, utc_now, sep="\n")
+print(kati, utc_now, sep="\n")
