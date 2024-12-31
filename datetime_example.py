acd88ab502c78edef7d9e855d04cf75395a6ce91
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-12-31 12:13:56 +0300

    datetine and aliases

--- datetime_example.py
+++ datetime_example.py
@@ -1,5 +1,9 @@
-import datetime
+import datetime as date_objects
 from datetime import datetime
 
-today = datetime.datetime.today()
-print(today)
+today = datetime.today()
+kati = datetime.now()
+utc_now = datetime.utcnow()
+_time = date_objects.time.fromisoformat("11:30")
+
+print(today, _time, kati, utc_now, sep="\n")
