73f5c76972e33d71b777fe8bed1bd3afa8177a65
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-02 12:02:21 +0300

    Protected & Private

--- banking.py
+++ banking.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Account:
     """ Simple account class """
 
@@ -8,12 +11,22 @@ class Account:
     def show_balance(self):
         print(f"The balance is {self.__balance}")
 
-
     def describe(self):
         self.show_balance()
+        self.__show_date()
+
+    @staticmethod
+    def __show_date():  # private
+        print(datetime.datetime.now())
+
+    @staticmethod
+    def _show_something():  # protected
+        pass
 
 
 dfcu = Account("Maria", 50000000)
 dfcu.show_balance()
 dfcu.balance = 0
 Account.show_balance(dfcu)
+Account.describe(dfcu)
+Account._show_something()
