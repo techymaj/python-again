6ad9d0e94fdcce930583c50f3e85e75125a57a05
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-03 00:55:08 +0300

    Calling super methods

--- inheritance_intro.py
+++ inheritance_intro.py
@@ -1,5 +1,6 @@
 from typing import override
 
+
 class Species:
     @staticmethod
     def species():
@@ -20,12 +21,12 @@ class Dog(Animal, Species):
 
     @override
     def walk(self):
+        # self.walk()  # recursion error. method calling itself
+        # Animal.walk()
+        super(Dog, self).walk()  # calling super methods
+        super().walk()  # calling super methods. preferred
         print(f"{self._name} walks")
 
-    def eat(self, speed):
-        print(f"{self._name} eats very {speed}")
-
-    @overload
     def eat(self, speed, style):
         print(f"{self._name} eats very {speed} {style}")
 
@@ -33,5 +34,4 @@ class Dog(Animal, Species):
 dog = Dog("Rex")
 dog.walk()
 dog.species()
-dog.eat("slowly")
 dog.eat("fast", "chewing")
