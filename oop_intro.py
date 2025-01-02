b1ff974d6e8fa85ca6599de1e5cde578cf8e93dd
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-02 14:47:30 +0300

    Getters

--- oop_intro.py
+++ oop_intro.py
@@ -1,9 +1,19 @@
 class Animal:
 
     def __init__(self, name):
+        self.age = 2
         self.name = name
         self.alive = True
 
 
+    def how_old(self, age):
+        pass
+
+    def get_name(self) -> str:
+        if self.age > 1:
+            return self.name
+
+
 dog = Animal("Rex")
 print(f"{dog.name} is {'alive' if dog.alive else 'dead' }")
+print(dog.get_name())
