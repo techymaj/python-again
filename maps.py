683a0ba1eca2691ea678d74012353d8be1e5f017
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-05 02:50:59 +0300

    Maps not as performant as comprehensions

--- maps.py
+++ maps.py
@@ -1,10 +1,17 @@
-atlas = map(lambda x: x ** 2, [1, 2, 4, 6, 8])
-print(atlas)
-
-for obj in atlas:
-    print(obj)
+from timeit import timeit
 
 
-power = map(str.upper, "i am")
-for obj in power:
-    print(obj, end="")
+def at():
+    atlas = map(lambda x: x ** 2, [1, 2, 4, 6, 8])
+    return [*atlas]
+
+
+def proj_at():
+    return [x ** 2 for x in [1, 2, 4, 6, 8]]
+
+
+time_atlas = timeit(at, number=10_000_000)
+time_project_atlas = timeit(proj_at, number=10_000_000)
+
+print(time_atlas)
+print(time_project_atlas)
