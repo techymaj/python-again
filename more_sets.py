639dc06cfc9de5baca4d6ba95efd0694b2109012
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-05 02:02:06 +0300

    Timeit Module

--- more_sets.py
+++ more_sets.py
@@ -1,3 +1,5 @@
+from timeit import timeit
+
 mixology = {
     1,
     "me",
@@ -6,4 +8,30 @@ mixology = {
     (3, 4, 5)
 }
 
-print(mixology)
+
+def transform(param: set):
+    new_set = {*""}
+    for item in param:
+        new_set.update({str(item)})
+    return new_set
+
+
+def transform_comp(param: set):
+    return {str(item) for item in param}
+
+
+transformed = transform(mixology)
+transformed_comp = transform_comp(mixology)
+print(transformed)
+print(transformed_comp)
+
+timed = timeit("transform(mixology)",
+               "from __main__ import mixology, transform",
+               number=100)
+
+timed_comp = timeit("transform_comp(mixology)",
+                    "from __main__ import mixology, transform_comp",
+                    number=100)
+
+print(timed)
+print(timed_comp)
