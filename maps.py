003b0fd826c2068e70018370f723dec560a926de
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-05 03:07:13 +0300

    Maps not as performant as comprehensions

--- maps.py
+++ maps.py
@@ -10,8 +10,8 @@ def proj_at():
     return [x ** 2 for x in [1, 2, 4, 6, 8]]
 
 
-time_atlas = timeit(at, number=10_000_000)
-time_project_atlas = timeit(proj_at, number=10_000_000)
+time_atlas = timeit(at, number=100_000_000)
+time_project_atlas = timeit(proj_at, number=100_000_000)
 
 print(time_atlas)
 print(time_project_atlas)
